8a38e67ef054ae1d817cda2bca912009b5022e4b
tharindu.wick <tharindu.wickramasinghe-ext@harver.com>
2020-10-11 23:15:28 +0530

    sqs recieve message --in progress

--- lambdas/flowMapper/index.js
+++ lambdas/flowMapper/index.js
@@ -1,39 +1,31 @@
 // Load the AWS SDK for Node.js
-var AWS = require("aws-sdk");
+const AWS = require("aws-sdk");
 // Set the region
 AWS.config.update({ region: "us-east-2" });
 
 // Create an SQS service object
-var sqs = new AWS.SQS({ apiVersion: "2012-11-05" });
-
-var params = {
-  // Remove DelaySeconds parameter and value for FIFO queues
-  DelaySeconds: 10,
-  MessageAttributes: {
-    Title: {
-      DataType: "String",
-      StringValue: "The Whistler",
-    },
-    Author: {
-      DataType: "String",
-      StringValue: "John Grisham",
-    },
-    WeeksOn: {
-      DataType: "Number",
-      StringValue: "6",
-    },
-  },
-  MessageBody:
-    "Information about current NY Times fiction bestseller for week of 12/11/2016.",
-  // MessageDeduplicationId: "TheWhistler",  // Required for FIFO queues
-  // MessageGroupId: "Group1",  // Required for FIFO queues
-  QueueUrl: "https://sqs.us-east-2.amazonaws.com/317127958808/flow-queue",
-};
+const sqs = new AWS.SQS({ apiVersion: "2012-11-05" });
 
 exports.handler = async function (event, context) {
-  console.log("Flow tiggered");
+  console.log("Lambda tiggered");
+  const params = {
+    // Remove DelaySeconds parameter and value for FIFO queues
+    DelaySeconds: 0,
+    MessageAttributes: {
+      Title: {
+        DataType: "String",
+        StringValue: "The Whistler",
+      },
+    },
+    MessageBody: `Current time ${new Date().toLocaleString()}`,
+    // MessageDeduplicationId: "TheWhistler",  // Required for FIFO queues
+    // MessageGroupId: "Group1",  // Required for FIFO queues
+    QueueUrl: "https://sqs.us-east-2.amazonaws.com/317127958808/flow-queue",
+  };
+
   const response = await sqs.sendMessage(params).promise();
   console.log(`SQS Response: ${JSON.stringify(response)}`);
+  console.log("Lambda completed");
   return {
     statusCode: 200,
     body: JSON.stringify(response),
--- lambdas/sqsTrigger/index.js
+++ lambdas/sqsTrigger/index.js
@@ -1,4 +1,28 @@
+// Load the AWS SDK for Node.js
+var AWS = require("aws-sdk");
+// Set the region
+AWS.config.update({ region: "us-east-2" });
+
+// Create an SQS service object
+var sqs = new AWS.SQS({ apiVersion: "2012-11-05" });
+
+var queueURL = "https://sqs.us-east-2.amazonaws.com/317127958808/flow-queue";
+
+var params = {
+  AttributeNames: ["SentTimestamp"],
+  MaxNumberOfMessages: 10,
+  MessageAttributeNames: ["All"],
+  QueueUrl: queueURL,
+  VisibilityTimeout: 20,
+  WaitTimeSeconds: 0,
+};
+
 exports.handler = async function (event, context) {
-  console.log("EVENT: \n" + JSON.stringify(event, null, 2));
-  return context.logStreamName;
+  const messages = await sqs.receiveMessage(params).promise();
+  console.log(JSON.stringify(messages));
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({}),
+  };
 };
